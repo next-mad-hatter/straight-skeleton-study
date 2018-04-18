9f188320978afdf0e620356703f806e4905957da
Max Deineko <max.deineko@gmail.com>
2018-04-18 18:43:50 +0200

    Kotton: file reading & triangulation

--- kotton/src/main/kotlin/main.kt
+++ kotton/src/main/kotlin/main.kt
@@ -2,7 +2,52 @@ package madhat.kotton
 
 import  madhat.kotton.utils.*
 
+import org.poly2tri.*
+import org.poly2tri.geometry.polygon.*
+//import org.poly2tri.triangulation.delaunay.*
+
+import java.util.*
+
+import java.io.*
+import org.tukaani.xz.*
+
+
 fun main(args: Array<String>) {
+
+    /*
+     * We must not have points with same coordinates here.
+     * Also, only simple polygons are supported.
+     */
+    var polygon = Polygon(Arrays.asList(
+            PolygonPoint(0.0, 0.0, 0.0),
+            PolygonPoint(10.0, 0.0, 1.0),
+            PolygonPoint(10.0, 10.0, 2.0),
+            PolygonPoint(0.0, 10.0, 3.0)))
+    Poly2Tri.triangulate(polygon)
+    for (t in polygon.getTriangles()) {
+        println(t)
+    }
+
+    /*
+     * XZ File reading
+     */
+    if (!args.isEmpty()) {
+        val file = File(args[0])
+        try {
+            val a = XZInputStream(FileInputStream(file)).reader()
+            val b = BufferedReader(a)
+            b.useLines {
+                it.forEach { println(it) }
+            }
+        }
+        catch (e: XZFormatException) {
+            FileInputStream(file).bufferedReader().useLines {
+                it.forEach { println(it) }
+            }
+        }
+    }
+
     val sandbox = Sandbox()
     sandbox.run()
+
 }
--- kotton/src/main/kotlin/utils/sandbox.kt
+++ kotton/src/main/kotlin/utils/sandbox.kt
@@ -1,9 +1,23 @@
 package madhat.kotton.utils
 
+import java.math.*
+
 class Sandbox {
+
+    fun add(x: BigDecimal, y: BigDecimal): BigDecimal = x + y
+
+    // following is not legal apparently
+    // val prn1 = print
+    fun prn2(x: String) = print(x)
+
+    var a = 1
 
     fun run() {
         println("Hello World!")
+        val x = "Interpolating $a"
+        a = 2
+        println(x)
+        println("Interpolating $a")
     }
 
 }
