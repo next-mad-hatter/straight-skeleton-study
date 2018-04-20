b0f66e44e93d3bf58b1250cc884f3a3755f42e35
Max Deineko <max.deineko@gmail.com>
2018-04-20 19:53:05 +0200

    Kotton calling campskeleton works

--- kotton/src/main/kotlin/main.kt
+++ kotton/src/main/kotlin/main.kt
@@ -12,6 +12,9 @@ import org.twak.camp.Output.*
 import javax.vecmath.Point3d
 import org.twak.utils.collections.Loop
 import java.time.chrono.JapaneseEra.values
+import java.time.chrono.JapaneseEra.values
+
+
 
 
 
@@ -47,68 +50,31 @@ fun main(args: Array<String>) {
         poly.coordinates.mapValues({ x -> Pair(x.value.first.toBigDecimal(), x.value.second.toBigDecimal()) })
         */
 
-        println("\n  Campskeleton Test:\n")
-        // This runs fine
-
-        val c1 = Corner(0.0, 0.0)
-	    val c2 = Corner(100.0, -100.0)
-        val c3 = Corner(100.0, 0.0)
-		val speed1 = Machine(Math.PI/4)
-        val speed2 = Machine(Math.PI/3)
-		val loop1: Loop<Edge> = Loop()
-		val e1 = Edge(c1, c2)
-        val e2 = Edge(c2, c3)
-        val e3 = Edge(c3, c1)
-		loop1.append(e1)
-		loop1.append(e2)
-		loop1.append(e3)
-		e1.machine = speed1
-		e2.machine = speed1
-		e3.machine = speed2
-		val skel = Skeleton( loop1.singleton(), true )
-		skel.skeleton()
-        for (face in skel.output.faces.values) {
-            println("face:")
-            for (lp3 in face.points)
-                for (pt in lp3)
-                    println(pt)
-        }
-
+        // TODO: remove consequtive collinear edges for campskeleton
 
         println("\n  Campskeleton:\n")
 
-        /*
-        // FIXME: crashes
         var loop: Loop<Edge> = Loop()
-        for (ind in 0 until poly.sortedIndices.count()) {
-            val v = poly.sortedIndices[ind]
-            val w = poly.sortedIndices[(ind + 1) % poly.sortedIndices.count()]
-            val p = Corner(poly.coordinates[v]!!.first, poly.coordinates[v]!!.second)
-            val q = Corner(poly.coordinates[w]!!.first, poly.coordinates[w]!!.second)
+        val corners = poly.sortedIndices.map {
+            Corner( poly.coordinates[it]!!.first, poly.coordinates[it]!!.second)
+        }
+
+        for (ind in 0 until corners.count()) {
+            val p = corners[ind]
+            val q = corners[(ind + 1) % poly.sortedIndices.count()]
             val e = Edge(p, q)
-            println("Appending $e")
             loop.append(e)
-            val machine = Machine(Math.PI/4) // TODO: set weight
-            e.machine = machine
+            e.machine = Machine(Math.PI/4 * poly.weights[ind]) // TODO: set weight properly
         }
         val skeleton = Skeleton(loop.singleton(), true)
-        */
-
-        // FIXME: also crashes
-        var loop: Loop<Corner> = Loop()
-        for (ind in poly.sortedIndices) {
-            val c = Corner(poly.coordinates[ind]!!.first, poly.coordinates[ind]!!.second)
-            println("Appending $c")
-            loop.append(c)
-        }
-        val skeleton = Skeleton(loop.singleton())
-
-        /*
         skeleton.skeleton()
-        println(skeleton.output.edges)
-        for (e in skeleton.output.faces.values) {
+
+        for (face in skeleton.output.faces.values) {
+            println("  face:")
+            for (lp3 in face.points)
+                for (pt in lp3)
+                    println("  $pt")
         }
-        */
     }
 
 }
